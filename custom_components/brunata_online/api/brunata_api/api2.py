b2abf9f27b9cf52d6b5e184542ba7a46b7826c9a
Thomas Hoffmann <thoms_hoffmann@me.com>
2025-12-28 15:43:56 +0100

    switch from urandom to secrets for getting random bytes during auth

--- custom_components/brunata_online/api/brunata_api/api2.py
+++ custom_components/brunata_online/api/brunata_api/api2.py
@@ -4,6 +4,7 @@ import hashlib
 import logging
 import os
 import re
+import secrets
 import urllib.parse
 from socket import gaierror
 
@@ -87,7 +88,7 @@ class BrunataApi:
         # aiohttp.CookieJar(unsafe=True)
 
         # Initialize challenge values
-        code_verifier = base64.urlsafe_b64encode(os.urandom(40)).decode("utf-8")
+        code_verifier = base64.urlsafe_b64encode(secrets.token_bytes(40)).decode("utf-8")
         code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
         code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
         code_challenge = (
